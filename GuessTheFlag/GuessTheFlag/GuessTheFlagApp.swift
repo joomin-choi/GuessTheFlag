835e7822297e3a13efb5a38612f6eb74344d3186
JooMin Choi <joomin.choi@clearscore.com>
2022-08-30 11:00:18 +0100

    initial commit

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by JooMin Choi on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
